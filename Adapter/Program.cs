821fbb6c0fcd87f2cfb64c0230b98685206a82d5
cduffy-tri <caseyduffy00@gmail.com>
2026-04-19 18:22:06 -0500

    add adapter pattern

---
+++ Adapter/Program.cs
@@ -0,0 +1,35 @@
+﻿public class XeroxPrinter
+{
+    public void PrintDocument(string document)
+    {
+        Console.WriteLine($"Printing Document {document}");
+    }
+}
+
+public interface IPrinter
+{
+    void Print(string document);
+}
+
+public class XeroxPrinterAdapter : IPrinter
+{
+    private XeroxPrinter xeroxPrinter;
+    public XeroxPrinterAdapter(XeroxPrinter xeroxPrinter)
+    {
+        this.xeroxPrinter = xeroxPrinter;
+    }
+
+    public void Print(string docuemnt)
+    {
+        xeroxPrinter.PrintDocument(docuemnt);
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        IPrinter printer = new XeroxPrinterAdapter(new XeroxPrinter());
+        printer.Print("hello");
+    }
+}
