bd520d0e9196ec294f5ab087b35d50702c137791
ramzy2000 <caseyduffy00@gmail.com>
2026-06-25 14:47:57 -0500

    add documentation for adapter pattern

--- Adapter/Program.cs
+++ Adapter/Program.cs
@@ -1,4 +1,9 @@
-﻿public class Program
+﻿// our program needs to use the IPrinter interface however the library
+// has its own printer driver called XeronPrinter. We create a IPrinter named
+// XeronPrinterAdapter this contains the class we are adapting and plugs into
+// the IPrinter class.
+
+public class Program
 {
     public static void Main(string[] args)
     {
